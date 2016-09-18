0a14acaeb3d2d4467df943bac832121b622baf18
Luko van der Maas <lukovandermaas@gmail.com>
2016-09-18 12:04:19 +0200

    finished first version and added summaries

--- MNIST.py
+++ MNIST.py
@@ -8,23 +8,85 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_integer("MINI_BATCH_SIZE", 100, "size of the mini-batch")
 flags.DEFINE_integer("TEST_STEPS", 10, "amount of steps before testing")
+flags.DEFINE_string("summaries_dir", "/tmp/NN-summaries")
+
+
+if tf.gfile.Exists(FLAGS.summaries_dir):
+    tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
+tf.gfile.MakeDirs(FLAGS.summaries_dir)
 
 
 with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1], name="data")
+    tf.image_summary('input', x, 10)
     y_ = tf.placeholder(tf.float32, shape=[None, 10], name="labels")
     keep_prob = tf.placeholder(tf.float32)
 
 with tf.name_scope("conv1"):
-    conv1 = conv_layer(x, [5, 5, 1, 50], [1, 1, 1, 1], "VALID")
+    conv1 = conv_layer(x, [5, 5, 1, 64], [1, 1, 1, 1], "VALID", "conv1")
 
 with tf.name_scope("conv2"):
-    conv2 = conv_layer(conv1, [5, 5, 50, 80], [1, 1, 1, 1], "VALID")
+    conv2 = conv_layer(conv1, [5, 5, 64, 128], [1, 1, 1, 1], "VALID", "conv2")
 
 with tf.name_scope("fully_connected"):
-    conv2_reshapen = tf.reshape(conv2, [None, 720])  # TODO: dubble check 720
-    fully_connected1 = nn_layer(conv2_reshapen, 720, 500)
-    dropout1 = tf.nn.dropout(fully_connected1, keep_prob)
+    conv2_reshapen = tf.reshape(conv2, [None, 20 * 20 * 128])
+    fc_1 = nn_layer(conv2_reshapen, 20 * 20 * 80, 1024, "fc_1")
+    fc_1_dropout = tf.nn.dropout(fc_1, keep_prob)
 
 with tf.name_scope("output_layer"):
-    y = nn_layer(dropout1, 720, 10, act=tf.nn.softmax)
+    y = nn_layer(fc_1_dropout, 1024, 10, "output", act=tf.nn.softmax)
+
+with tf.name_scope("loss"):
+    loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    tf.scalar_summary("loss", loss)
+
+with tf.name_scope("train"):
+    trainer = tf.train.AdamOptimizer()
+    train_step = trainer.minimize(loss)
+
+with tf.name_scope("accuracy"):
+    with tf.name_scope('correct_prediction'):
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    with tf.name_scope('accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.scalar_summary('accuracy', accuracy)
+
+
+def feed_dict(train):
+    """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
+    if train:
+        xs, ys = mnist.train.next_batch(100)
+        k = FLAGS.dropout
+    else:
+        xs, ys = mnist.test.images, mnist.test.labels
+        k = 1.0
+    return {x: xs, y_: ys, keep_prob: k}
+
+with tf.Session() as sess:
+    merged = tf.merge_all_summaries()
+    train_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/train', sess.graph)
+    test_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/test')
+
+    sess.run(tf.initialize_all_variables())
+
+    for i in range(FLAGS.max_steps):
+        if i % 10 == 0:  # Record summaries and test-set accuracy
+            summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
+            test_writer.add_summary(summary, i)
+            print('Accuracy at step %s: %s' % (i, acc))
+        else:  # Record train set summaries, and train
+            if i % 100 == 99:  # Record execution stats
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step],
+                                      feed_dict=feed_dict(True),
+                                      options=run_options,
+                                      run_metadata=run_metadata)
+                train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
+                train_writer.add_summary(summary, i)
+                print('Adding run metadata for', i)
+            else:  # Record a summary
+                summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+                train_writer.add_summary(summary, i)
+    train_writer.close()
+    test_writer.close()
--- utility.py
+++ utility.py
@@ -1,7 +1,20 @@
 import tensorflow as tf
 
 
-def conv_layer(input_tensor, filter_shape, strides, padding):
+def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.scalar_summary('mean/' + name, mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.scalar_summary('sttdev/' + name, stddev)
+        tf.scalar_summary('max/' + name, tf.reduce_max(var))
+        tf.scalar_summary('min/' + name, tf.reduce_min(var))
+        tf.histogram_summary(name, var)
+
+
+def conv_layer(input_tensor, filter_shape, strides, padding, layer_name, act=tf.nn.relu):
     """
     a convolutional layer initializer
 
@@ -9,6 +22,8 @@ def conv_layer(input_tensor, filter_shape, strides, padding):
     :param filter_shape: [filter_height, filter_width, in_channels, out_channels]
     :param strides: list of ints defining the stride
     :param padding: "SAME" / "VALID"
+    :param layer_name: name of the layer
+    :param act: the activation function used
     :return: shape [batch, in_height, in_width, in_channels]
     """
 
@@ -19,18 +34,21 @@ def conv_layer(input_tensor, filter_shape, strides, padding):
             initializer=tf.contrib.layers.xavier_initializer_conv2d(),
             trainable=True
         )
+        tf.image_summary(layer_name + "/kernels")
     with tf.name_scope('biases'):
-        biases = tf.Variable(tf.constant(1.0, shape=filter_shape[3]))
-
+        biases = tf.constant(1.0, shape=filter_shape[3])
+        variable_summaries(biases, layer_name + '/biases')
     with tf.name_scope('convolution'):
         # http://stackoverflow.com/questions/34619177/what-does-tf-nn-conv2d-do-in-tensorflow
-        pre_bias_activation = tf.nn.conv2d(input_tensor, kernels, strides, padding)
-        activation = tf.nn.bias_add(pre_bias_activation, biases)
+        preactivation = tf.nn.conv2d(input_tensor, kernels, strides, padding)
+        tf.image_summary(layer_name + "/preactivations")
+        activation = act(preactivation) + biases
+        tf.image_summary(layer_name + "/activations")
 
     return activation
 
 
-def nn_layer(input_tensor, input_dim, output_dim, act=tf.nn.relu):
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
     """Reusable code for making a simple neural net layer.
 
     It does a matrix multiply, bias add, and then uses relu to nonlinearize.
@@ -45,9 +63,13 @@ def nn_layer(input_tensor, input_dim, output_dim, act=tf.nn.relu):
             initializer=tf.contrib.layers.xavier_initializer(),
             trainable=True
         )
+        variable_summaries(weights, layer_name + '/weights')
     with tf.name_scope('biases'):
-        biases = tf.Variable(tf.constant(0.1, shape=output_dim))
+        biases = tf.constant(0.1, shape=output_dim)
+        variable_summaries(biases, layer_name + '/biases')
     with tf.name_scope('Wx_plus_b'):
         preactivate = tf.matmul(input_tensor, weights) + biases
+        tf.histogram_summary(layer_name + '/pre_activations', preactivate)
     activations = act(preactivate, 'activation')
+    tf.histogram_summary(layer_name + '/activations', activations)
     return activations
